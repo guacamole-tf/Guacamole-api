a8e04507c3eb670785faf087354bec1ba4f3f7be
kwj1270 <kwj1270@naver.com>
2022-11-19 19:46:37 +0900

    GM-0005 test: 상품 도메인, 재고는 0 미만의 개수로 생성할 수 없다.

---
+++ src/test/kotlin/com/guacamole/api/production/domain/product/StockTest.kt
@@ -0,0 +1,22 @@
+package com.guacamole.api.production.domain.product
+
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+
+
+internal class StockTest {
+    @Test
+    fun `재고는 0 이하의 개수로 생성할 수 없다`() {
+        assertThrows<RuntimeException> {
+            createStock(-1)
+        }
+    }
+
+    fun createStock(
+        count: Int = 0,
+        id: Long = 1
+    ): Stock {
+        return Stock(count, id)
+    }
+}
+
