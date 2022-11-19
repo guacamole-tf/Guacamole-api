28191e9e77bfa2be7b196b95e89a4e8e92d1ab86
kwj1270 <kwj1270@naver.com>
2022-11-19 20:35:22 +0900

    GM-0005 feat: 상품 도메인, 카테고리의 상위 카테고리 번호가 0 미만이라면 예외를 발생한다

--- src/main/kotlin/com/guacamole/api/production/domain/product/Category.kt
+++ src/main/kotlin/com/guacamole/api/production/domain/product/Category.kt
@@ -2,10 +2,15 @@ package com.guacamole.api.production.domain.product
 
 class Category(
     var name: String,
-    var parentId: Long? = null,
+    var parentId: Long = 0,
     var id: Long? = null
 ) {
     init {
         require(name.isNotBlank())
+        require(parentId.get(0))
     }
+}
+
+fun Long.get(other: Long): Boolean {
+    return this >= other
 }
