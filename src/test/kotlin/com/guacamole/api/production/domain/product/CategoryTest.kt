3af9a15fc71dec47c434a9b17f81d5add951b93f
kwj1270 <kwj1270@naver.com>
2022-11-19 20:31:38 +0900

    GM-0005 test: 상품 도메인, 카테고리의 상위 카테고리 번호가 0 미만이라면 예외를 발생한다

--- src/test/kotlin/com/guacamole/api/production/domain/product/CategoryTest.kt
+++ src/test/kotlin/com/guacamole/api/production/domain/product/CategoryTest.kt
@@ -9,9 +9,17 @@ class CategoryTest {
 
     @ParameterizedTest
     @ValueSource(strings = ["", " ", "   ", "    "])
-    fun `카테고리 이름이 공백으로만 이루어져 있다면 에러를 발생한다`(name: String) {
+    fun `카테고리의 이름이 공백으로만 이루어져 있다면 에러를 발생한다`(name: String) {
         assertThrows<RuntimeException> {
             createCategory(name)
+        }
+    }
+
+    @ParameterizedTest
+    @ValueSource(longs = [-1, -2, -10, -100, Long.MIN_VALUE])
+    fun `카테고리의 상위 카테고리 번호가 0 미만이라면 예외를 발생한다`(parentId: Long) {
+        assertThrows<RuntimeException> {
+            createCategory(parentId = parentId)
         }
     }
 
