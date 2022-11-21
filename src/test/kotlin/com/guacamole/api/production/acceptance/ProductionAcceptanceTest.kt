aedd920f96657e961817a7277afc3df5a949d5ad
kwj1270 <kwj1270@naver.com>
2022-11-22 02:48:10 +0900

    GM-0005 test: 상품 도메인, 정상적인 상품 수정 데이터를 보내면 상품이 수정된다.

--- src/test/kotlin/com/guacamole/api/production/acceptance/ProductionAcceptanceTest.kt
+++ src/test/kotlin/com/guacamole/api/production/acceptance/ProductionAcceptanceTest.kt
@@ -3,15 +3,18 @@ package com.guacamole.api.production.acceptance
 import com.guacamole.api.production.acceptance.steps.CategorySteps.`카테고리 생성됨`
 import com.guacamole.api.production.acceptance.steps.CategorySteps.`카테고리를 생성 요청됨`
 import com.guacamole.api.production.acceptance.steps.CategorySteps.`카테고리를 생성 요청을 한다`
+import com.guacamole.api.production.acceptance.steps.ProductSteps.`상품 생성 요청 됨`
 import com.guacamole.api.production.acceptance.steps.ProductSteps.`상품 생성 요청을 한다`
 import com.guacamole.api.production.acceptance.steps.ProductSteps.`상품 생성됨`
+import com.guacamole.api.production.acceptance.steps.ProductSteps.`상품 수정 요청을 한다`
+import com.guacamole.api.production.acceptance.steps.ProductSteps.`상품 수정됨`
 import com.guacamole.api.production.fixture.CATEGORY_NAME
 import com.guacamole.api.production.fixture.CATEGORY_PARENT_ID
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 
 @DisplayName("상품 인수 테스트")
-class ProductionAcceptanceTest: AcceptanceTest() {
+class ProductionAcceptanceTest : AcceptanceTest() {
 
     /**
      * Scenario: 삼품 관리
@@ -29,8 +32,11 @@ class ProductionAcceptanceTest: AcceptanceTest() {
         val categoryCreateResponse = `카테고리를 생성 요청을 한다`(CATEGORY_NAME, CATEGORY_PARENT_ID)
         `카테고리 생성됨`(categoryCreateResponse)
 
-        val response = `상품 생성 요청을 한다`(1)
-        `상품 생성됨`(response)
+        val productCreateResponse = `상품 생성 요청을 한다`(1)
+        `상품 생성됨`(productCreateResponse)
+
+        val productUpdateResponse = `상품 수정 요청을 한다`(1)
+        `상품 수정됨`(productUpdateResponse)
     }
 
     @Test
@@ -40,5 +46,15 @@ class ProductionAcceptanceTest: AcceptanceTest() {
         val response = `상품 생성 요청을 한다`(categoryId)
 
         `상품 생성됨`(response)
+    }
+
+    @Test
+    fun `상품을 수정한다`() {
+        val categoryId = `카테고리를 생성 요청됨`(CATEGORY_NAME, CATEGORY_PARENT_ID)
+        val productId = `상품 생성 요청 됨`(categoryId)
+
+        val response = `상품 수정 요청을 한다`(productId)
+
+        `상품 수정됨`(response)
     }
 }
--- src/test/kotlin/com/guacamole/api/production/acceptance/steps/ProductSteps.kt
+++ src/test/kotlin/com/guacamole/api/production/acceptance/steps/ProductSteps.kt
@@ -38,6 +38,39 @@ object ProductSteps {
             .extract()
     }
 
+    fun `상품 수정 요청을 한다`(
+        categoryId: Long = PRODUCT_CATEGORY_ID,
+        name: String = PRODUCT_NAME,
+        descriptionImagePath: String = PRODUCT_DESCRIPTION_IMAGE_PATH,
+        originPlace: String = PRODUCT_ORIGIN_PLACE,
+        detailDescription: String = PRODUCT_DETAIL_DESCRIPTION,
+    ): ExtractableResponse<Response> {
+        val params = `상품 수정 데이터를 만든다`(categoryId, name, descriptionImagePath, originPlace, detailDescription)
+        return RestAssured.given().log().all()
+            .accept(MediaType.APPLICATION_JSON_VALUE)
+            .contentType(ContentType.JSON)
+            .body(params)
+            .`when`().put(PRODUCT_PATH)
+            .then().log().all()
+            .extract()
+    }
+
+    private fun `상품 수정 데이터를 만든다`(
+        categoryId: Long,
+        name: String,
+        descriptionImagePath: String,
+        originPlace: String,
+        detailDescription: String
+    ): Map<String, String> {
+        return mapOf(
+            PRODUCT_CATEGORY_ID_FIELD to categoryId.toString(),
+            PRODUCT_NAME_FIELD to name,
+            PRODUCT_DESCRIPTION_IMAGE_PATH_FIELD to descriptionImagePath,
+            PRODUCT_ORIGIN_PLACE_FIELD to originPlace,
+            PRODUCT_DETAIL_DESCRIPTION_FIELD to detailDescription
+        )
+    }
+
     private fun `상품 생성 데이터를 만든다`(
         categoryId: Long,
         name: String,
@@ -67,7 +100,25 @@ object ProductSteps {
         )
     }
 
+    fun `상품 생성 요청 됨`(
+        categoryId: Long = PRODUCT_CATEGORY_ID,
+        name: String = PRODUCT_NAME,
+        thumbnailImagePath: String = PRODUCT_THUMBNAIL_IMAGE_PATH,
+        descriptionImagePath: String = PRODUCT_DESCRIPTION_IMAGE_PATH,
+        originPlace: String = PRODUCT_ORIGIN_PLACE,
+        detailDescription: String = PRODUCT_DETAIL_DESCRIPTION,
+    ): Long {
+        val response = `상품 생성 요청을 한다`(categoryId, name, thumbnailImagePath, descriptionImagePath, originPlace, detailDescription)
+        val header = response.header("location")
+        val removePrefix = header.removePrefix(CATEGORY_PATH).removePrefix("/")
+        return removePrefix.toLong()
+    }
+
     fun `상품 생성됨`(response: ExtractableResponse<Response>) {
         AssertionsForInterfaceTypes.assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
+    }
+
+    fun `상품 수정됨`(response: ExtractableResponse<Response>) {
+        AssertionsForInterfaceTypes.assertThat(response.statusCode()).isEqualTo(HttpStatus.NO_CONTENT.value());
     }
 }
