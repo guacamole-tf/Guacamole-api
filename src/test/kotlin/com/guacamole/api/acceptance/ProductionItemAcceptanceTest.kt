22d09f88a5d21fdbb1fabe605ab1112f16402b54
kwj1270 <kwj1270@naver.com>
2022-11-26 14:03:33 +0900

    GM-0005 test: 상품 도메인, 정상적인 상품 아이템 수정 데이터를 보내면 상품 아이템이 수정된다.

--- src/test/kotlin/com/guacamole/api/acceptance/ProductionItemAcceptanceTest.kt
+++ src/test/kotlin/com/guacamole/api/acceptance/ProductionItemAcceptanceTest.kt
@@ -3,12 +3,17 @@ package com.guacamole.api.acceptance
 import com.guacamole.api.acceptance.steps.CategorySteps.`카테고리 생성 요청 됨`
 import com.guacamole.api.acceptance.steps.CategorySteps.`카테고리 생성됨`
 import com.guacamole.api.acceptance.steps.CategorySteps.`카테고리를 생성 요청을 한다`
+import com.guacamole.api.acceptance.steps.ProductItemSteps.`상품 아이템 생성 요청 됨`
 import com.guacamole.api.acceptance.steps.ProductItemSteps.`상품 아이템 생성 요청을 한다`
 import com.guacamole.api.acceptance.steps.ProductItemSteps.`상품 아이템 생성됨`
+import com.guacamole.api.acceptance.steps.ProductItemSteps.`상품 아이템 수정 요청을 한다`
+import com.guacamole.api.acceptance.steps.ProductItemSteps.`상품 아이템 수정됨`
 import com.guacamole.api.acceptance.steps.ProductSteps.`상품 생성 요청 됨`
 import com.guacamole.api.acceptance.steps.ProductSteps.`상품 생성 요청을 한다`
 import com.guacamole.api.acceptance.steps.ProductSteps.`상품 생성됨`
 import com.guacamole.api.fixture.*
+import io.restassured.response.ExtractableResponse
+import io.restassured.response.Response
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 
@@ -40,5 +45,31 @@ class ProductionItemAcceptanceTest : AcceptanceTest() {
         )
 
         `상품 아이템 생성됨`(response)
+    }
+
+    @Test
+    fun `상품 아이템을 수정한다`() {
+        val categoryId = `카테고리 생성 요청 됨`(CATEGORY_NAME, CATEGORY_ROOT_PARENT_ID)
+        val productId = `상품 생성 요청 됨`(categoryId)
+
+        val productItemId = `상품 아이템 생성 요청 됨`(
+            productId,
+            PRODUCT_ITEM_PRICE,
+            PRODUCT_ITEM_COUNT,
+            PRODUCT_ITEM_THUMBNAIL_IMAGE_PATH,
+            PRODUCT_ITEM_SIZE_UNIT,
+            PRODUCT_SIZE_RATE
+        )
+
+        val response = `상품 아이템 수정 요청을 한다`(productItemId,
+            productId,
+            PRODUCT_ITEM_PRICE,
+            PRODUCT_ITEM_COUNT,
+            PRODUCT_ITEM_THUMBNAIL_IMAGE_PATH,
+            PRODUCT_ITEM_SIZE_UNIT,
+            PRODUCT_SIZE_RATE
+        )
+
+        `상품 아이템 수정됨`(response)
     }
 }
--- src/test/kotlin/com/guacamole/api/acceptance/steps/ProductItemSteps.kt
+++ src/test/kotlin/com/guacamole/api/acceptance/steps/ProductItemSteps.kt
@@ -32,6 +32,25 @@ object ProductItemSteps {
             .extract()
     }
 
+    fun `상품 아이템 수정 요청을 한다`(
+        productItemId: Long = PRODUCT_ITEM_ID,
+        productId: Long = PRODUCT_ITEM_PRODUCT_ID,
+        price: Int = PRODUCT_ITEM_PRICE,
+        count: Int = PRODUCT_ITEM_COUNT,
+        thumbnailImagePath: String = PRODUCT_ITEM_THUMBNAIL_IMAGE_PATH,
+        sizeUnit: String = PRODUCT_ITEM_SIZE_UNIT,
+        sizeRate: Int = PRODUCT_SIZE_RATE
+    ): ExtractableResponse<Response> {
+        val params = `상품 수정 데이터를 만든다`(price, count, thumbnailImagePath, sizeUnit, sizeRate)
+        return RestAssured.given().log().all()
+            .accept(MediaType.APPLICATION_JSON_VALUE)
+            .contentType(ContentType.JSON)
+            .body(params)
+            .`when`().put(PRODUCT_ITEM_PATH.format(productId) + "/${productItemId}")
+            .then().log().all()
+            .extract()
+    }
+
     /**
      * Create Request Data
      */
@@ -53,10 +72,45 @@ object ProductItemSteps {
         )
     }
 
+    private fun `상품 수정 데이터를 만든다`(
+        price: Int = PRODUCT_ITEM_PRICE,
+        count: Int = PRODUCT_ITEM_COUNT,
+        thumbnailImagePath: String = PRODUCT_ITEM_THUMBNAIL_IMAGE_PATH,
+        sizeUnit: String = PRODUCT_ITEM_SIZE_UNIT,
+        sizeRate: Int = PRODUCT_SIZE_RATE
+    ): Map<String, String> {
+        return mapOf(
+            PRODUCT_ITEM_PRICE_FIELD to price.toString(),
+            PRODUCT_ITEM_COUNT_FIELD to count.toString(),
+            PRODUCT_ITEM_THUMBNAIL_IMAGE_PATH_FIELD to thumbnailImagePath,
+            PRODUCT_ITEM_SIZE_UNIT_FIELD to sizeUnit,
+            PRODUCT_ITEM_SIZE_RATE_FIELD to sizeRate.toString()
+        )
+    }
+
+    fun `상품 아이템 생성 요청 됨`(
+        productId: Long = PRODUCT_ITEM_PRODUCT_ID,
+        price: Int = PRODUCT_ITEM_PRICE,
+        count: Int = PRODUCT_ITEM_COUNT,
+        thumbnailImagePath: String = PRODUCT_ITEM_THUMBNAIL_IMAGE_PATH,
+        sizeUnit: String = PRODUCT_ITEM_SIZE_UNIT,
+        sizeRate: Int = PRODUCT_SIZE_RATE
+    ): Long {
+        val response = `상품 아이템 생성 요청을 한다`(productId, price, count, thumbnailImagePath, sizeUnit, sizeRate)
+        val header = response.header("location")
+        val removePrefix = header.removePrefix(PRODUCT_ITEM_PATH.format(productId)).removePrefix("/")
+        return removePrefix.toLong()
+    }
+
+
     /**
      * Response Assertion
      */
     fun `상품 아이템 생성됨`(response: ExtractableResponse<Response>) {
         AssertionsForInterfaceTypes.assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
+    }
+
+    fun `상품 아이템 수정됨`(response: ExtractableResponse<Response>) {
+        AssertionsForInterfaceTypes.assertThat(response.statusCode()).isEqualTo(HttpStatus.NO_CONTENT.value());
     }
 }
