d0c4882d8291d0e168bf110d1ca81c181fa10671
kwj1270 <kwj1270@naver.com>
2022-11-26 19:21:47 +0900

    GM-0005 test: 상품 도메인, 인수/통합 테스트를 위한 DataBaseCleanUp 추가

--- src/test/kotlin/com/guacamole/api/acceptance/AcceptanceTest.kt
+++ src/test/kotlin/com/guacamole/api/acceptance/AcceptanceTest.kt
@@ -1,10 +1,12 @@
 package com.guacamole.api.acceptance
 
 import io.restassured.RestAssured
+import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.web.server.LocalServerPort
-
+import org.springframework.context.annotation.Profile
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class AcceptanceTest {
@@ -12,8 +14,12 @@ class AcceptanceTest {
     @LocalServerPort
     var port = 0
 
+    @Autowired
+    private lateinit var dataBaseCleanUp: DataBaseCleanUp
+
     @BeforeEach
     fun setUp() {
         RestAssured.port = port
+        dataBaseCleanUp.execute()
     }
 }
